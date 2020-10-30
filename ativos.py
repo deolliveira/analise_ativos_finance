a3ae95670635abc181794670e8aaf5a5e2e56b19
deolliveira <deolliveira@gmail.com>
2020-10-30 15:10:43 -0300

    First commit

--- ativos.py
+++ ativos.py
@@ -4,15 +4,14 @@ import datetime as dt
 
 Acao_1 = 'PETR4.SA'
 Acao_2 = 'COGN3.SA'
-Acao_3 = 'CIEL3.SA'
-Acao_4 = 'SHOW3.SA'
+Acao_3 = 'IRBR3.SA'
 
-start = '2019-01-01'
+start = '2020-03-01'
 end = dt.datetime.today()
 
 price = pd.DataFrame()
 
-Tickers = [Acao_1, Acao_2, Acao_3, Acao_4]
+Tickers = [Acao_1, Acao_2, Acao_3]
 
 for i in Tickers:
     price[i] = pdr.DataReader(i, 'yahoo', start, end)['Adj Close']
